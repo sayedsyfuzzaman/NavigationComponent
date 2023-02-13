9396bed803b3153c11b9379789c9b57b193de6b8
sayedsyfuzzaman <sayedsyfuzzaman@gmail.com>
2023-02-13 21:29:58 +0600

    Navigation Component

---
+++ app/src/main/java/com/example/navigationcomponent/FirstFragment.kt
@@ -0,0 +1,28 @@
+package com.example.navigationcomponent
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.navigationcomponent.databinding.ActivityMainBinding
+import com.example.navigationcomponent.databinding.FragmentFirstBinding
+
+class FirstFragment : Fragment() {
+
+    private lateinit var binding: FragmentFirstBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentFirstBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.helloTextView.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.navigateToSecond)
+        }
+        return view
+    }
+}
---
+++ app/src/main/java/com/example/navigationcomponent/SecondFragment.kt
@@ -0,0 +1,29 @@
+package com.example.navigationcomponent
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.navigationcomponent.databinding.FragmentFirstBinding
+import com.example.navigationcomponent.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+    private lateinit var binding: FragmentSecondBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentSecondBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.thankYouTextView.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.navigateToFirst)
+        }
+
+        return view
+    }
+
+}
