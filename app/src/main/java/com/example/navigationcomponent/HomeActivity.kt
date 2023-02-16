4ed35a33d9b5f35631f76dff5ebbd90cbbca3dd3
sayedsyfuzzaman <sayedsyfuzzaman@gmail.com>
2023-02-16 10:57:58 -0800

    Add IDs and Map with bottom nav

--- app/src/main/java/com/example/navigationcomponent/HomeActivity.kt
+++ app/src/main/java/com/example/navigationcomponent/HomeActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +19,9 @@ class HomeActivity : AppCompatActivity() {
         fragment.arguments = bundle
         supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView, fragment).commit()
 
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNav)
+        val navController = findNavController(R.id.fragmentContainerView)
+        bottomNavigationView.setupWithNavController(navController)
 
     }
 
