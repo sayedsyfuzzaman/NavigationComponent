819acf5e7cb69fe18b2b9bc779e06a129baaf901
sayedsyfuzzaman <sayedsyfuzzaman@gmail.com>
2023-02-19 13:20:00 -0800

    Navbar and bottom navigation in same activity bug fixes

--- app/src/main/java/com/example/navigationcomponent/HomeActivity.kt
+++ app/src/main/java/com/example/navigationcomponent/HomeActivity.kt
@@ -25,13 +25,18 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val fragment = HomeFragment()
+        val bundle = Bundle()
+        bundle.putString("name", intent.getStringExtra("name"))
+        fragment.arguments = bundle
+        supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment_content_main, fragment).commit()
+
         setSupportActionBar(binding.appBarMain.toolbar)
 
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         appBarConfiguration = AppBarConfiguration(
             navController.graph, drawerLayout
         )
@@ -39,22 +44,15 @@ class HomeActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
 
 
-        val fragment = HomeFragment()
-        val bundle = Bundle()
-        bundle.putString("name", intent.getStringExtra("name"))
-        fragment.arguments = bundle
-        supportFragmentManager.beginTransaction().replace(R.id.nav_host_fragment_content_main, fragment).commit()
-
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNav)
         bottomNavigationView.setupWithNavController(navController)
-
     }
 
-//    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        menuInflater.inflate(R.menu.bottom_menu, menu)
-//        return true
-//    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.bottom_menu, menu)
+        return true
+    }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
